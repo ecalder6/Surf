d499a62b886bf1e4dea49ad64659fd24b71d17dc
Greg Beerbower <grbee@microsoft.com>
2018-07-25 11:16:46 -0700

    Better poll questions were added

--- FlightsQuery.cs
+++ FlightsQuery.cs
@@ -8,16 +8,16 @@
     {
         // TODO see if we need something like this to get this read
         // speak: SSMLHelper.Speak()
-        [Prompt("Please enter your origin")]
+        [Prompt("Please tell me which city or airport you are leaving from?")]
         public string Origin { get; set; }
 
-        [Prompt("Please enter your destination")]
+        [Prompt("Please tell me which city or airport you are headed to?")]
         public string Destination { get; set; }
         
         [Prompt("What date do you want to leave?")]
         public DateTime DepartDate { get; set; }
 
-        [Prompt("When do you want to return?")]
+        [Prompt("What date do you want to return?")]
         public DateTime ReturnDate { get; set; }
     }
 }
